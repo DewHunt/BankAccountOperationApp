fdf210d7ac39277b406fe7ad463781e70e6759da
s-sabbir <dew.fog1553@gmail.com>
2015-05-13 16:05:38 +0600

    https://github.com/s-sabbir/BankAccountOperationApp.git

---
+++ BankAccountOperationApp/Account.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountOperationApp
+{
+    class Account
+    {
+        public string AccountNumber;
+        public string CustomerName;
+        public double Balance;
+
+        
+        public string WithdrawMoney(double amount)
+        {
+            if (AccountNumber != null || CustomerName != null)
+            {
+                if (amount > 0)
+                {
+                    if (Balance > amount)
+                    {
+                        Balance -= amount;
+                        return "Successfull";
+                    }
+                    else
+                    {
+                        return "Insufficient Balance";
+                    }
+                }
+                else
+                {
+                    return "Enter Valid Amount";
+                }
+            }
+            else
+            {
+                return "Please Create Account";
+            }
+        }
+
+        public string DepositeMoney(double amount)
+        {
+            if (AccountNumber != null || CustomerName != null)
+            {
+                if (amount > 0)
+                {
+                    Balance += amount;
+                    return "Successfull";
+                }
+                else
+                {
+                    return "Enter Valid Amount";
+                }
+            }
+            else
+            {
+                return "Please Create Account";
+            }
+        }
+
+        public string Report()
+        {
+            return CustomerName + ", Your Balance is : " + Balance;
+        }
+    }
+}
---
+++ BankAccountOperationApp/AccountOperationUI.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankAccountOperationApp
+{
+    public partial class AccountOperationUI : Form
+    {
+        public AccountOperationUI()
+        {
+            InitializeComponent();
+        }
+
+        Account aAccount = new Account();
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            if (aAccount.AccountNumber != null || aAccount.CustomerName != null)
+            {
+                aAccount.AccountNumber = accountNumberTextBox.Text;
+                aAccount.CustomerName = customerNameTextBox.Text;
+
+                MessageBox.Show("Account created Successfulluy");
+
+                accountNumberTextBox.Clear();
+                customerNameTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please Create Account");
+            }
+        }
+
+        private void depositButton_Click(object sender, EventArgs e)
+        {
+            double amount = Convert.ToDouble(amountTextBox.Text);
+
+            string message = aAccount.DepositeMoney(amount);
+
+            MessageBox.Show(message);
+        }
+
+        private void withdrawButton_Click(object sender, EventArgs e)
+        {
+            double amount = Convert.ToDouble(amountTextBox.Text);
+
+            string message = aAccount.WithdrawMoney(amount);
+
+            MessageBox.Show(message);
+        }
+
+        private void reportButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(aAccount.Report());
+        }
+    }
+}
